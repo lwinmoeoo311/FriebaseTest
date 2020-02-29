8e7678c2dcef7679f5d496fb52a9d51dc786638c
lwinmoeoo311 <lwinmoeoo3112@gmail.com>
2020-03-01 02:22:15 +0630

    FireBaseTest

---
+++ app/src/main/java/com/lmo/firebasetest/LoginActivity.kt
@@ -0,0 +1,89 @@
+package com.lmo.firebasetest
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_activity.*
+
+class LoginActivity: AppCompatActivity() {
+
+    private lateinit var base : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        base = FirebaseAuth.getInstance()
+
+//        base.signInWithEmailAndPassword(mail_tv.toString(), password_tv.toString())
+//            .addOnCompleteListener(this) {
+//                if (it.isSuccessful) {
+//                    base.currentUser
+//                    chgActivity()
+//                }
+//            }
+
+        login_btn.setOnClickListener { login() }
+
+        logout_btn.setOnClickListener { signUp() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        base.currentUser
+
+        onStop()
+    }
+
+    private fun login() {
+        if (mail_tv.text.isNullOrBlank() || password_tv.text.isNullOrBlank()) {
+            Toast.makeText(this, "* marked fields are required to fill", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            base.signInWithEmailAndPassword(mail_tv.text.toString(), password_tv.text.toString())
+                .addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        base.currentUser
+                        Toast.makeText(this, "${mail_tv.text}", Toast.LENGTH_SHORT).show()
+                        chgActivity()
+                    }
+                }
+        }
+    }
+
+    private fun signUp() {
+        if (mail_tv.text.isNullOrBlank() || password_tv.text.isNullOrBlank()) {
+            Toast.makeText(this, "* marked fields are required to fill", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            base.createUserWithEmailAndPassword(mail_tv.text.toString(), password_tv.text.toString())
+                .addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        base.currentUser
+                        chgActivity()
+                    } else {
+                        Log.e(TAG, "${it.exception?.message}")
+                        Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener {
+                    Log.e(TAG, "${it.message}")
+                    Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun chgActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private var TAG = LoginActivity::class.java.simpleName
+    }
+}
--- app/src/main/java/com/lmo/firebasetest/MainActivity.kt
+++ app/src/main/java/com/lmo/firebasetest/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.lmo.firebasetest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var base: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        base = FirebaseAuth(FirebaseApp.getInstance())
+
+        out_btn.setOnClickListener { logout() }
+    }
+
+    private fun logout() {
+        base.signOut()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        onStop()
     }
 }
